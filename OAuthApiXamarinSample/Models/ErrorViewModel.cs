94656fb12e2168b83737793fa4e92118bb176880
Munir Husseini <m.husseini@gmx.de>
2017-09-03 11:34:22 +0200

    Add project files.

---
+++ OAuthApiXamarinSample/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace OAuthApiXamarinSample.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
